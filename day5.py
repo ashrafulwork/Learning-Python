82d495b56492aecc9f60e2adcd6cf8a151e2dc79
Md Ashraful Hossain <ashrafulhossainwork@gmail.com>
2023-08-14 10:49:13 +0600

    init

--- day5.py
+++ day5.py
@@ -1,2 +1,16 @@
-print("Day 5 of learning Python")
-print("Comments")
+
+# # print("Day 5 of learning Python")
+
+# # print("Comments, Escape Sequences & Print Statement")
+# # print("Welcome El Professor")
+# # press Ctrl + slash key (/)
+
+
+# """ GGWP"""
+
+# # print("Escape \nstatement Check")
+# # print("Escape \"statement\" \nCheck")
+
+print("This is a program \"to check the \" \nescape Sequences")
+print("Hey", 6, 7, sep="`", end="47\n")
+print("El Professor")
