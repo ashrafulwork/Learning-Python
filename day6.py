5092e8a1a61895655f6c896d954a55b11bb3c9a7
Md Ashraful Hossain <ashrafulhossainwork@gmail.com>
2023-08-18 00:47:22 +0600

    update

---
+++ day6.py
@@ -0,0 +1,9 @@
+# a = "137"
+# b = "Ashraful"
+# # result = a + b
+# # print(result)
+# print(type(a))
+# list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+# print(list1)
+x = {"name": "John", "age": 36}
+print(type(x))
---
+++ day7+day8.py
@@ -0,0 +1,28 @@
+# exercise
+
+a = 100
+b = 37
+result = 0
+
+# Part - Addition
+result = a + b
+print("Addition:", result)
+
+
+# Part - Subtraction
+result = a - b
+print("Subtraction:", result)
+
+
+# Part - Multiplication
+result = a * b
+print("Multiplication:", result)
+
+
+# Part - Division
+result = a / b
+print("Division:", result)
+
+# Part - Floor Division
+result = a // b
+print("Floor Division:", result)
