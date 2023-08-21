f2324263ac57248eb1d0077dcdd1b8c73a1bea2b
Md Ashraful Hossain <ashrafulhossainwork@gmail.com>
2023-08-21 20:03:55 +0600

    update

--- day11.py
+++ day11.py
@@ -1,5 +1,42 @@
-introduction = '''
-Hey guys ! This is El Professor.
-Today we are here today to learn how to write multiple string lines 
-'''
-print(introduction)
+# -- part one starts from here -- #
+
+# introduction = '''
+# Hey guys ! This is El Professor.
+# Today we are here today to learn how to write multiple string lines
+# '''
+# print(introduction)
+
+
+# -- part two starts from here -- #
+# serial_one = [137, 33, 11]
+# serial_two = "Hossain"
+# print(serial_one[1])
+
+
+# intro = "Hi Myself Md Ashraful Hossain"
+
+for testing_loops in intro:
+    print(testing_loops)
+
+
+# Testing what i have learned till now ( didnt learned for loop yet )
+# user_input = input(("Do you wish to continue ? Type Y or N  "));
+
+# # if user_input!="Y" or user_input!="y":
+
+# #       print("You have pressed wrong input")
+# #       print("Ok...Discontinuing");
+
+# if user_input=="N" or user_input=="n" in user_input:
+#     print("You have pressed N '")
+#     print("Ok... Discontinuing")
+
+# elif user_input=="Y" or user_input=="y" in user_input :
+#     first_name  = input("Enter your first name : ");
+#     second_name  = input("Enter your second name : ");
+#     email = input("Enter your email account : ")
+#     print("Name : ",first_name,second_name,"\nEmail Address :",email);
+
+# elif user_input!="Y" or user_input!="y"  or user_input!="N" or user_input!="n"  in user_input :
+#     print("Wrong Input")
+#     print("Discontinuing");
